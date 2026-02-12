1aa4ba9d8ff1160da27bde72cf1f1518ebb76638
MartinCupak <martin.cupak@gmail.com>
2026-02-12 15:49:19 +0800

    new/modif build script

---
+++ build_cuda.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+echo "Building the Blink correlator for CUDA..."
+
+[ -d build ] || mkdir build
+cd build
+cmake .. -DBLINK_TEST_DATADIR="~/data/blink-test-data" \
+         -DUSE_CUDA=ON \
+         -DUSE_OPENMP=OFF \
+         -DCMAKE_CUDA_ARCHITECTURES="61-real" \
+         -DMAKE_BUILD_TYPE=Release \
+         -DCMAKE_C_COMPILER=nvcc \
+         -DCMAKE_CXX_COMPILER=nvcc \
+         -DCMAKE_CXX_FLAGS="-O3"
+
+make -j 8 VERBOSE=1
+sudo make install
--- env_cuda.sh
+++
@@ -1,5 +0,0 @@
-#/usr/bin/bash
-
-export BLINK_TEST_DATADIR="/home/mcu/data/blink-test-data"
-export CMAKE_CUDA_ARCHITECTURES="61-real"
-export CMAKE_BUILD_TYPE=Release
