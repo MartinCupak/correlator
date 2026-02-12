19c967f10004c871020e83a70b2a49bc412dff76
MartinCupak <martin.cupak@gmail.com>
2026-02-12 18:23:56 +0800

    add optimisation

--- tests/build_tests_cuda.sh
+++ tests/build_tests_cuda.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-nvcc correlation_test.cpp -o correlation_test  -I/usr/local/include -I/usr/local/cuda-11.8/include -L/usr/local/lib -L/usr/local/cuda-11.8/lib64 -lpsrdada -lcudart -lm -lpthread -lblink_astroio -lcorrelation
+nvcc correlation_test.cpp -o correlation_test -O3 -I/usr/local/include -I/usr/local/cuda-11.8/include -L/usr/local/lib -L/usr/local/cuda-11.8/lib64 -lpsrdada -lcudart -lm -lpthread -lblink_astroio -lcorrelation
